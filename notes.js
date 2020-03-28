76a726e6d1f179ba2d731e6a0d6869f2fa2398ab
Tanveer Singh <tanveer.singh926@gmail.com>
2020-03-28 20:35:31 +0800

    add debugger

--- notes.js
+++ notes.js
@@ -5,8 +5,9 @@ const getNotes = () => "Your notes...";
 const addNote = (title, body) => {
   const notes = loadNotes();
   // const duplicateNotes = notes.filter(note => note.title === title);
-  const duplicateNote = notes.find(note => note.title === title);
 
+  const duplicateNote = notes.find(note => note.title === title);
+  debugger;
   if (!duplicateNote) {
     notes.push({
       title: title,
