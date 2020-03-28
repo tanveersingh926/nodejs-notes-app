64d6f3a2bb52bac37a3c318a78dd5c372ef8ef00
Tanveer Singh <tanveer.singh926@gmail.com>
2020-03-28 19:24:11 +0800

    add yargs and setup command

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-const validator = require("validator");
+const yargs = require("yargs");
 const chalk = require("chalk");
 const getNotes = require("./notes.js");
 const msg = getNotes();
 
-console.log(msg);
-console.log(chalk.green.bold.inverse(msg));
+yargs.version("1.1.0");
+
+// Create a add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log("Adding a new note");
+  }
+});
+
+// Create a remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove note",
+  handler: function() {
+    console.log("Removing a note");
+  }
+});
+
+// Create a list command
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: function() {
+    console.log("List all notes");
+  }
+});
+
+// Create a read command
+yargs.command({
+  command: "read",
+  describe: "Read notes",
+  handler: function() {
+    console.log("Read notes");
+  }
+});
+
+console.log(yargs.argv);
